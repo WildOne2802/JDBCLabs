fe7f26b75f1ce183fd8ab5407c155e4b0ea3fd1b
napenshe <mr.oshnokov@gmail.com>
2020-05-06 21:34:22 +0300

    ITEMDAO fix needed

---
+++ src/JDBCOracle.java
@@ -0,0 +1,5 @@
+public class JDBCOracle {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/JDBCTest.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.derby.jdbc.ClientDriver;
+
+public class JDBCTest {
+    public static void main(String[] args) {
+        try {
+            Connection conn = null;
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/JavaTunesDB");
+
+            DatabaseMetaData dbmd = conn.getMetaData();
+            System.out.println(dbmd.getDriverName());
+            System.out.println(dbmd.getUserName());
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
