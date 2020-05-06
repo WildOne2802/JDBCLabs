202951ec4cb85650b7f4aa32e740a1f07da8e714
napenshe <mr.oshnokov@gmail.com>
2020-05-06 21:27:38 +0300

    first commit

---
+++ src/com/javatunes/util/ItemDAOMain.java
@@ -0,0 +1,19 @@
+package com.javatunes.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ItemDAOMain {
+    public static void main(String[] args) throws SQLException {
+        MusicItem mi;
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:derby://localhost:1527/JavaTunesDB");
+        ItemDAO itemDAO = new ItemDAO(conn);
+        mi =  itemDAO.searchById(1L);
+        mi.toString();
+        mi = itemDAO.searchById(100L);
+        mi.toString();
+
+
+    }
+}
